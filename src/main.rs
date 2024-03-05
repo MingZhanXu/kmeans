c15729c1c71b8f34bb99ce9bd0955f527e6fd333
MingZhanXu <MingZhan.Xu.vm3@gmail.com>
2024-03-05 16:08:53 +0800

    暫時存檔，嘗試增加交握

--- src/main.rs
+++ src/main.rs
@@ -73,6 +73,7 @@ enum MessageType {
     TeamMessage((usize, usize, Vec<Vec<usize>>)), //step, num.
     KNumMessage((usize, usize, Vec<usize>)),
     ResetKNumMessage(Vec<usize>),
+    HandshakingMessage((String, usize)), //Port, MessageType(now_step)
 }
 #[derive(Serialize, Deserialize, Debug)]
 struct TaskUser {
@@ -207,32 +208,22 @@ fn kmeans(
         let mut seed_num: usize = 0;
         let mut last_k_num: Vec<usize> = Vec::new();
         let mut point_flag = false;
-        let mut user_list: Vec<usize>;
+        let mut user_list: Vec<usize> = Vec::new();
         let mut user_list_flag: usize = 0;
         let mut max_user: usize = 0;
-
+        
         let mut step_now = 0;
         let mut start_time: Instant = Instant::now();
         let mut user_id: TaskUser = TaskUser {
             code_name: 0,
             num: 0,
         };
+        // let mut my_addr: &str;
         loop {
-            println!("準備接收訊息");
-            (_buf, size) = receive_long_message(&socket_get);
+            // println!("準備接收訊息");
+            (_buf, size) = receive_message(&socket_get);
             let msg_type: MessageType =
             serde_json::from_slice(&_buf[..size]).expect("Failed to deserialize message");
-            let m: MessageType =
-            serde_json::from_slice(&_buf[..size]).expect("Failed to deserialize message");
-
-            match m {
-                MessageType::PointMessage(_points) =>{
-                    println!("接收到Point");
-                }
-                _ => {
-                    println!("---");
-                }
-            }
             match msg_type {
                 //接收到任務
                 MessageType::TaskNameMessage(get_task, get_max) => {
@@ -244,14 +235,14 @@ fn kmeans(
                     match num{
                         TASK_PUBLLISHER => {
                             println!("發布者");
-                            let msg_type = MessageType::CodeNameMessage(user_id.code_name);
-                            send_message(&socket_send, msg_type);
                         }
                         _ => {
                             println!("接取者");
                             user_id.code_name = rand::thread_rng().gen_range(2..std::usize::MAX as usize);
                         }
                     }
+                    let msg_type = MessageType::CodeNameMessage(user_id.code_name);
+                    send_message(&socket_send, msg_type);
                 }
                 //接收到使用者代號
                 MessageType::CodeNameMessage(get_code_name) => {
@@ -260,14 +251,15 @@ fn kmeans(
                         //任務發布者
                         TASK_PUBLLISHER => {
                             if user_list_flag < max_user {
-                                if user_list_flag == 0{
-                                    user_list[TASK_PUBLLISHER] = TASK_PUBLLISHER;
-                                    user_list_flag = 1;
-                                }
                                 user_list[user_list_flag] = get_code_name;
                                 user_list_flag += 1;
+                                println!("user_list_flag:{} max_user:{}",user_list_flag ,max_user);
                                 if user_list_flag == max_user{
-                                    let msg_type = MessageType::NumMessage(user_list);
+                                    let msg_type = MessageType::NumMessage(user_list.clone());
+                                    send_message(&socket_send, msg_type);
+                                    thread::sleep(Duration::from_secs(1));
+                                    let msg_type =
+                                        MessageType::PointMessage(generate_point(dot_num)); //產生隨機點
                                     send_message(&socket_send, msg_type);
                                 }
                             } else {
@@ -285,11 +277,6 @@ fn kmeans(
                     println!("set user_list");
                     println!("{:?}", get_user_list);
                     user_list = get_user_list;
-                    if num == TASK_PUBLLISHER{
-                        let msg_type =
-                            MessageType::PointMessage(generate_point(dot_num)); //產生隨機點
-                        send_message(&socket_send, msg_type);
-                    }
                 }
                 //Point
                 MessageType::PointMessage(points) => {
@@ -301,8 +288,11 @@ fn kmeans(
                         step_now = 1;
                         //隨機中心點
                         if user_id.num == 0 {
+                            println!("發送初始中心點");
+                            let mut k_num = thread_k_num.lock().unwrap();
+                            *k_num = random_center(seed_num_temp, dot_num);
                             let msg_type =
-                                MessageType::ResetKNumMessage(random_center(seed_num_temp, dot_num)); //發送初始中心點
+                                MessageType::ResetKNumMessage(k_num.clone()); //發送初始中心點
                             send_message(&socket_send, msg_type);
                         }
                     } else {
@@ -314,6 +304,7 @@ fn kmeans(
                     println!("reset k_num step_now:{}", step_now);
                     if user_id.num == TASK_PUBLLISHER{
                         start_time = Instant::now();
+                        println!("****Set start_time");
                     }
                     if point_flag == true {
                         let mut k_num = thread_k_num.lock().unwrap();
@@ -326,6 +317,7 @@ fn kmeans(
                         step_now += 1;
                         let msg_type =
                             MessageType::TeamMessage((step_now, user_id.num, team_list[user_id.num].clone())); //發送team
+                        println!("-------add step; step_now:{}\nmsg_type:{:?}", step_now, msg_type);
                         send_message(&socket_send, msg_type);
                     } else {
                         println!("無法接收k_num，需等到處理完畢");
@@ -334,10 +326,14 @@ fn kmeans(
                 //群資料
                 MessageType::TeamMessage((get_step, get_num, get_team)) => {
                     println!("received team step_now:{}", step_now);
+                    println!("get_team:{:?}", get_team);
+                    println!("get_step:{} step_now:{}", get_step, step_now);
                     if point_flag == true && get_step == step_now && team_flag < max_user {
+                        println!("team_flag:{}", team_flag);
                         team_list[get_num] = get_team.clone();
                         println!("get_num:{}\nget_team:{:?}", get_num, get_team.clone());
                         team_flag += 1;
+                        println!("team_flag:{} max_user:{}", team_flag, max_user);
                         if team_flag == max_user {
                             team_flag = 0;
                             let mut team = thread_team.lock().unwrap();
@@ -346,7 +342,9 @@ fn kmeans(
                             for _i in 0..seed_num {
                                 team.push(Vec::new());
                             }
+                            println!("team_list:{:?}", team_list);
                             for i in 0..max_user {
+                                println!("team_list[{}].len():{}", i, team_list[i].len());
                                 for j in 0..seed_num {
                                     //j會超出
                                     team[j].extend(&team_list[i][j]);
@@ -408,6 +406,10 @@ fn kmeans(
                         println!("{} {}", k_num_flag, seed_num);
                     }
                 }
+                //
+                MessageType::HandshakingMessage((get_str, get_type)) => {
+
+                }
             }
         }
     });
@@ -417,12 +419,8 @@ fn kmeans(
         let socket = UdpSocket::bind("0.0.0.0:0").expect("Failed to bind socket");
         let task = rand::thread_rng().gen_range(0..std::usize::MAX as usize);
         let msg_type = MessageType::TaskNameMessage(task.to_string(), max); //產生隨機點
-        // for i in serialized_msg.chars(){
-        //     socket.send_to(i.to_string().as_bytes(), "127.0.0.1:8888").expect("Failed to send message");
-        // }
         send_message(&socket, msg_type);
         thread::sleep(Duration::from_secs(1));
-
     }
 
     handle.join().unwrap();
@@ -474,33 +472,22 @@ fn read_number(prompt: &str) -> usize {
         .expect("Failed to read line");
     input.trim().parse().expect("Invalid input")
 }
-//UDP發送訊息
-// fn send_message(socket_send: &UdpSocket, msg_type: MessageType) {
-//     send_long_message(socket_send, msg_type);
-//     // let serialized_msg = serde_json::to_string(&msg_type).expect("Failed to serialize message");
-//     // socket_send
-//     //     .send_to(serialized_msg.as_bytes(), "127.0.0.1:8888")
-//     //     .expect("Failed to send message");
-//     // socket_send
-//     //     .send_to(serialized_msg.as_bytes(), "127.0.0.1:8889")
-//     //     .expect("Failed to send message");
-// }
-
+//發送長訊息
 fn send_message(socket: &UdpSocket, message: MessageType) {
     let serialized_msg = serde_json::to_string(&message).expect("Failed to serialize message");
     let mut remaining = serialized_msg.as_bytes();
     let mut _offset = 0;
     while !remaining.is_empty() {
         let chunk = &remaining[..MAX_MESSAGE_SIZE.min(remaining.len())];
-        socket.send_to(chunk, "127.0.0.1:8889").expect("Failed to send message");
         socket.send_to(chunk, "127.0.0.1:8888").expect("Failed to send message");
+        socket.send_to(chunk, "127.0.0.1:8889").expect("Failed to send message");
         remaining = &remaining[chunk.len()..];
         _offset += chunk.len();
     }
-    println!("發送完畢");
+    // println!("發送完畢");
 }
-
-fn receive_long_message(socket: &UdpSocket) -> (Box<[u8]>, usize) {
+//接收長訊息
+fn receive_message(socket: &UdpSocket) -> (Box<[u8]>, usize) {
     let mut received_message = Vec::new();
     let mut total_bytes = 0;
     loop {
@@ -510,7 +497,7 @@ fn receive_long_message(socket: &UdpSocket) -> (Box<[u8]>, usize) {
                 total_bytes += received_bytes;
                 received_message.extend_from_slice(&buffer[..received_bytes]);
                 if received_bytes < MAX_MESSAGE_SIZE {
-                    println!("接收完畢");
+                    // println!("接收完畢");
                     break;
                 }
             }
@@ -522,15 +509,15 @@ fn receive_long_message(socket: &UdpSocket) -> (Box<[u8]>, usize) {
     }
     (received_message.into_boxed_slice(), total_bytes)
 }
-
+//自動獲取Port(單機測試)
 fn get_port(start_port: i32) -> Option<UdpSocket>{
     let mut receive_port = start_port;
-    let mut receive_socket = None;
+    let mut _receive_socket = None;
     loop {
         match UdpSocket::bind(format!("127.0.0.1:{}", receive_port)) {
             Ok(socket) => {
                 println!("Successfully bound to port {}", receive_port);
-                receive_socket = Some(socket);
+                _receive_socket = Some(socket);
                 break;
             }
             Err(_) => {
@@ -539,5 +526,5 @@ fn get_port(start_port: i32) -> Option<UdpSocket>{
             }
         }
     }
-    receive_socket
+    _receive_socket
 }
