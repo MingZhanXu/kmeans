91ea0e08cda710c1cd48a53da142ef1c14f7a6ec
MingZhanXu <MingZhan.Xu.vm3@gmail.com>
2024-03-08 06:00:49 +0800

    暫時存檔; kmeans.rs文檔案與測試添加完成

--- src/kmeans.rs
+++ src/kmeans.rs
@@ -1,5 +1,22 @@
 use crate::kmeans_struct::*;
-/// 產生隨機中心點
+/// 產生隨機群中心點
+///
+/// # 變數說明
+///
+/// * `seed_num`: 所要產生中心點數量。
+/// * `dot_num`: 座標總數。
+/// * `回傳` : `k_num`: k_num\[中心點順序\]
+/// # 使用範例
+///
+/// ```
+/// use kmeans::kmeans_struct::Point;
+/// use kmeans::kmeans::random_center;
+/// let seed_num = 5;
+/// let dot_num = 10;
+/// let result = random_center(seed_num, dot_num);
+/// assert_eq!(result.len(), seed_num);
+/// ```
+///
 pub fn random_center(seed_num: usize, dot_num: usize) -> Vec<usize> {
     let mut k_num: Vec<usize> = vec![];
     for _ in 0..seed_num {
@@ -15,7 +32,28 @@ pub fn random_center(seed_num: usize, dot_num: usize) -> Vec<usize> {
     }
     k_num
 }
-/// 將點位分群
+/// 將座標分群
+///
+/// # 變數說明
+///
+/// * `point`: point\[座標編號\]。
+/// * `k_num`: k_num\[中心點順序\]。
+/// * `num`: 使用者編號。
+/// * `max`: 總運算人數。
+/// * `回傳` : `team`: team\[使用者編號\]\[中心點順序\]
+/// # 使用範例
+///
+/// ```
+/// use kmeans::kmeans_struct::Point;
+/// use kmeans::kmeans::cluster;
+/// let point = vec![Point { x: 1.0, y: 2.0 }, Point { x: 3.0, y: 4.0 }, Point { x: 5.0, y: 6.0 }, Point { x: 8.0, y: 8.0}];
+/// let k_num = vec![2, 3];
+/// let num = 0;
+/// let max = 1;
+/// let result = cluster(&point, &k_num, num, max);
+/// assert_eq!(result, vec![vec![0, 1, 2], vec![3]]);
+/// ```
+///
 pub fn cluster(point: &Vec<Point>, k_num: &Vec<usize>, num: usize, max: usize) -> Vec<Vec<usize>> {
     let dot_num = point.len();
     let mut dot_range = dot_num / max;
@@ -47,7 +85,7 @@ pub fn cluster(point: &Vec<Point>, k_num: &Vec<usize>, num: usize, max: usize) -
 /// * `team`: team\[使用者編號\]\[中心點順序\]。
 /// * `num`: 使用者編號。
 /// * `max`: 總運算人數。
-///
+/// * `回傳` : k_num\[中心點順序\]
 /// # 使用範例
 ///
 /// ```
